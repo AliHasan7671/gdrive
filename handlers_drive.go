1adff33653d0e589dc03767c0ec4d82a847aa392
Ali Hasan <ahb7671@gmail.com>
2019-03-18 21:19:30 +0500

    use my own API
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "218394138638-1m66de10svtchl226dp1ptccr0n3uteq.apps.googleusercontent.com"
+const ClientSecret = "iatfm_PuuCVGcKpI2CJ2KI5-"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
